c71b28ea56c2cef44598807a290b1c593b6b78f3
piggybear <piggybear_net@163.com>
2017-10-10 11:25:45 +0800

    1、修改权限问题 2、修改可选类型修饰问题

--- PGImagePickerKingfisher/PGImagePickerKingfisher.swift
+++ PGImagePickerKingfisher/PGImagePickerKingfisher.swift
@@ -13,18 +13,18 @@ import PGImagePicker
 
 public class PGImagePickerKingfisher: PGImagePicker {
     
-    public var indicatorType: IndicatorType!
-    public var placeholder: UIImage!
-    public var imageUrls: [String]!
+    public var indicatorType: IndicatorType?
+    public var placeholder: UIImage?
+    public var imageUrls: [String]?
     
     override public func setupImageView(cell: PGCollectionViewCell, indexPath: IndexPath, imageView: UIImageView){
-        if imageUrls != nil && indexPath.row < imageUrls.count {
+        if imageUrls != nil && indexPath.row < (imageUrls?.count)! {
             let cellImageView: UIImageView =  cell.scrollView.imageView
-            let url = URL(string: self.imageUrls[indexPath.row])!
+            let url = URL(string: self.imageUrls![indexPath.row])!
             if self.indicatorType == nil {
                 cellImageView.kf.indicatorType = .activity
             }else {
-                cellImageView.kf.indicatorType = self.indicatorType
+                cellImageView.kf.indicatorType = self.indicatorType!
             }
             if self.placeholder == nil {
                 cellImageView.kf.setImage(with: url)
